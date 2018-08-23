3430df2566efb2cae4c6a16d523a3ff1b3d63521
trantuan <tincanxh@gmail.com>
2018-08-23 16:30:57 +0700

    add model

--- src/main/java/com/codegym/AppConfiguration.java
+++ src/main/java/com/codegym/AppConfiguration.java
@@ -4,8 +4,10 @@ package com.codegym;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -39,6 +41,13 @@ public class AppConfiguration implements ApplicationContextAware {
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
         viewResolver.setTemplateEngine(templateEngine());
         return viewResolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("ValidationMessages");
+        return messageSource;
     }
 
     @Override
